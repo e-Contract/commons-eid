cd5ff778595d9a829dba990671557f3aa07dfdda
Frank Marien <frank@apsu.be>
2013-05-27 13:57:16 +0200

    bumped all copyright dates to -2013

--- commons-eid-dialogs/src/main/java/be/fedict/eid/commons/dialogs/DefaultBeIDCardUI.java
+++ commons-eid-dialogs/src/main/java/be/fedict/eid/commons/dialogs/DefaultBeIDCardUI.java
@@ -1,6 +1,6 @@
 /*
  * Commons eID Project.
- * Copyright (C) 2008-2012 FedICT.
+ * Copyright (C) 2008-2013 FedICT.
  * Copyright (C) 2009 Frank Cornelis.
  *
  * This is free software; you can redistribute it and/or modify it
