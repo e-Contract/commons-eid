5471cb3e4c31074af8d96e9f5f48ccd2bf6a0ce8
Frank Cornelis <info@e-contract.be>
2021-05-21 18:00:16 +0200

    9765: EU and EU+ document types

--- commons-eid-consumer/src/main/java/be/fedict/commons/eid/consumer/DocumentType.java
+++ commons-eid-consumer/src/main/java/be/fedict/commons/eid/consumer/DocumentType.java
@@ -1,7 +1,7 @@
 /*
  * Commons eID Project.
  * Copyright (C) 2008-2013 FedICT.
- * Copyright (C) 2018-2020 e-Contract.be BV.
+ * Copyright (C) 2018-2021 e-Contract.be BV.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version
@@ -101,7 +101,11 @@ public enum DocumentType implements Serializable {
 
 	FOREIGNER_M("22"),
 
-	FOREIGNER_N("23");
+	FOREIGNER_N("23"),
+
+	FOREIGNER_EU("31"),
+
+	FOREIGNER_EU_PLUS("32");
 
 	private final int key;
 
